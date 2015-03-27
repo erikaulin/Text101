f8cfdc1f53b3b534096d601a10596e6008e39ae7
Erik Aulin <erik@ustwo.com>
2015-03-27 22:38:35 +0100

    sidewinder story

--- Assets/Scripts/TextController.cs
+++ Assets/Scripts/TextController.cs
@@ -6,185 +6,113 @@ public class TextController : MonoBehaviour {
 
 public Text text;
 private enum States	{
-	cell, mirror, sheets_0, lock_0, cell_mirror, sheets_1, lock_1, corridor_0, stairs_0, stairs_1,
-	stairs_2, courtyard, floor, corridor_1, corridor_2, corridor_3,closet_door, in_closet
+	ship, keyboard, look, button, flight, shield, navigation, drive, sol,
 	};
 
 private	States myState;
 
 // Use this for initialization
 void Start () {
-	myState = States.cell;
+	myState = States.ship;
 }
+
 
 // Update is called once per frame
 void Update () {
 	print (myState);
-	if (myState == States.cell) 								{cell();}
-	else if (myState == States.sheets_0) 				{sheets_0();}
-	else if (myState == States.sheets_1) 				{sheets_1();}
-	else if (myState == States.lock_0) 					{lock_0();}
-	else if (myState == States.lock_1) 					{lock_1();}
-	else if (myState == States.mirror) 					{mirror();}
-	else if (myState == States.cell_mirror) 		{cell_mirror();}
-	else if (myState == States.corridor_0) 			{corridor_0();}
-	else if (myState == States.stairs_0) 				{stairs_0();}
-	else if (myState == States.stairs_1) 				{stairs_1();}
-	else if (myState == States.stairs_2) 				{stairs_2();}
-	else if (myState == States.courtyard)				{courtyard();}
-	else if (myState == States.floor) 					{floor();}
-	else if (myState == States.corridor_1) 			{corridor_1();}
-	else if (myState == States.corridor_2) 			{corridor_2();}
-	else if (myState == States.corridor_3) 			{corridor_3();}
-	else if (myState == States.closet_door) 		{closet_door();}
-	else if (myState == States.in_closet) 			{in_closet();}
+	if (myState == States.ship) 								{ship();}
+	else if (myState == States.keyboard) 				{keyboard();}
+	else if (myState == States.look) 						{look();}
+	else if (myState == States.button) 					{button();}
+	else if (myState == States.flight) 					{flight();}
+	else if (myState == States.shield) 					{shield();}
+	else if (myState == States.navigation) 			{navigation();}
+	else if (myState == States.drive) 					{drive();}
+	else if (myState == States.sol) 						{sol();}
 }
 
 #region State handler methods
 
-void in_closet() {
-	text.text = "Inside the closet you see a cleaner's uniform that looks about your size! " +
-							"Seems like your day is looking-up.\n\n" +
-							"Press D to Dress up, or R to Return to the corridor";
-  if 			(Input.GetKeyDown(KeyCode.R)) 					{myState = States.corridor_2;}
-	else if (Input.GetKeyDown(KeyCode.D)) 		{myState = States.corridor_3;}
+void ship() {
+	text.text = "You wake up feeling groggy and distorted, waking up in zero gravity really messed with " +
+							"your orientation and you wonder for a few second where you are. The familiar sound of  " +
+							"the humming engine quickly remind you that you are in classic sidewinder. The last thing " +
+							"you remember was exiting hyperspace and the sun hit you dead on with a solar eruption.\n\n" +
+							"Press L to Look around";
+	if 			(Input.GetKeyDown(KeyCode.L)) 		{myState = States.look;}
 }
 
-void closet_door() {
-	text.text = "You are looking at a closet door, unfortunately it's locked. " +
-							"Maybe you could find something around to help enourage it open?\n\n" +
-							"Press R to Return to the corridor";
-  if (Input.GetKeyDown(KeyCode.R)) 					{myState = States.corridor_0;}
+
+void look() {
+	text.text = "Main holographic displays are not showing so the the cockpit is pretty dark. " +
+							"The only illumination are the stars outside and the emergency system " +
+							"restart Button behind the spectrum Keyboard.";
+  if 			(Input.GetKeyDown(KeyCode.B)) 		{myState = States.button;}
+	else if (Input.GetKeyDown(KeyCode.K)) 		{myState = States.keyboard;}
 }
 
-void corridor_3() {
-	text.text = "You're standing back in the corridor, now convincingly dressed as a cleaner. " +
-							"You strongly consider the run for freedom.\n\n" +
-							"Press S to take the Stairs, or U to Undress";
-	if 			(Input.GetKeyDown(KeyCode.S)) 		{myState = States.courtyard;}
-  else if (Input.GetKeyDown(KeyCode.U)) 		{myState = States.in_closet;}
+
+void button() {
+	text.text = "You push the restart Button multiple times but the holographic displays doesn't restart.\n\n" +
+							"Press L to Look around";
+  if (Input.GetKeyDown(KeyCode.L)) 					{myState = States.look;}
+	else if (Input.GetKeyDown(KeyCode.K)) 		{myState = States.keyboard;}
 }
 
-void corridor_2() {
-	text.text = "Back in the corridor, having declined to dress-up as a cleaner.\n\n" +
-							"Press C to revisit the Closet, and S to climb the stairs";
-  if 			(Input.GetKeyDown(KeyCode.C)) 		{myState = States.in_closet;}
-	else if (Input.GetKeyDown(KeyCode.S)) 		{myState = States.stairs_2;}
+
+void keyboard() {
+	text.text = "You look at the spectrum keyboard and remember the lecture your grandfather always held. " +
+							"- Every pilot should know the emergency Flight book by heart and the coordinates back to " +
+							"SOL and not trust these fancy holographics. ";
+  if (Input.GetKeyDown(KeyCode.F)) 					{myState = States.flight;}
+	else if (Input.GetKeyDown(KeyCode.K)) 		{myState = States.keyboard;}
 }
 
-void corridor_1() {
-	text.text = "Still in the corridor. Floor still dirty. Hairclip in hand. " +
-							"Now what? You wonder if that lock on the closet would succumb to " +
-							"to some lock-picking?\n\n" +
-							"P to Pick the lock, and S to climb the stairs";
-  if 			(Input.GetKeyDown(KeyCode.P)) 		{myState = States.in_closet;}
-	else if (Input.GetKeyDown(KeyCode.S)) 		{myState = States.stairs_1;}
+void flight() {
+	text.text = "You reach for the emergency Flight book and quickly see the page markers you attached " +
+							"during flight training so many years ago. Most of them are unreadable but you can make " +
+							"out some of them with the letters. S and N";
+  if (Input.GetKeyDown(KeyCode.L)) 					{myState = States.look;}
+	else if (Input.GetKeyDown(KeyCode.S)) 		{myState = States.shield;}
+	else if (Input.GetKeyDown(KeyCode.N)) 		{myState = States.navigation;}
 }
 
-void floor () {
-	text.text = "Rummagaing around on the dirty floor, you find a hairclip.\n\n" +
-							"Press R to Return to the standing, or H to take the Hairclip." ;
-  if 			(Input.GetKeyDown(KeyCode.R)) 		{myState = States.corridor_0;}
-	else if (Input.GetKeyDown(KeyCode.H)) 		{myState = States.corridor_1;}
+void shield() {
+	text.text = "As you fold to the page with the S marker page 212 Shield management. " +
+							"You quickly read through the page and punch in the commands on the spectrum Keyboard to " +
+							"restart shields module. ";
+  if (Input.GetKeyDown(KeyCode.L)) 					{myState = States.look;}
+	else if (Input.GetKeyDown(KeyCode.K)) 		{myState = States.keyboard;}
+	else if (Input.GetKeyDown(KeyCode.N)) 		{myState = States.navigation;}
 }
 
-void courtyard () {
-	text.text = "You walk through the courtyard dressed as a cleaner. " +
-							"The guard tips his hat at you as you waltz past, claiming " +
-							"your freedom. You heart races as you walk into the sunset.\n\n" +
-							"Press P to Play again." ;
-  if (Input.GetKeyDown(KeyCode.P)) 					{myState = States.cell;}
+void navigation() {
+	text.text = "As you fold to the page with the N marker it takes you to page 23 Navigation management. " +
+							"Your happy that your grandfather told you to always remember SOL coordinates. You punch  " +
+							"in the commands and coordinates on the spectrum Keyboard to SOL.\n" +
+							"Now you just need to find the page that talks about frameshift Drive.";
+  if (Input.GetKeyDown(KeyCode.L)) 					{myState = States.look;}
+	else if (Input.GetKeyDown(KeyCode.K)) 		{myState = States.keyboard;}
+	else if (Input.GetKeyDown(KeyCode.D)) 		{myState = States.drive;}
 }
 
-void stairs_0 () {
-	text.text = "You start walking up the stairs towards the outside light. " +
-							"You realise it's not break time, and you'll be caught immediately. " +
-							"You slither back down the stairs and reconsider.\n\n" +
-							"Press R to Return to the corridor." ;
-  if (Input.GetKeyDown(KeyCode.R)) 					{myState = States.corridor_0;}
+void drive() {
+	text.text = "You open page 121 frameshift Drive and start to punch in the commands on the spectrum " +
+							"Keyboard and push the SPACE key.";
+  if (Input.GetKeyDown(KeyCode.L)) 							{myState = States.look;}
+	else if (Input.GetKeyDown(KeyCode.K)) 				{myState = States.keyboard;}
+	else if (Input.GetKeyDown(KeyCode.N)) 				{myState = States.navigation;}
+	else if (Input.GetKeyDown(KeyCode.Space)) 		{myState = States.sol;}
 }
 
-void stairs_1 () {
-	text.text = "Unfortunately weilding a puny hairclip hasn't given you the " +
-							"confidence to walk out into a courtyard surrounded by armed guards!\n\n" +
-							"Press R to Retreat down the stairs" ;
-	if (Input.GetKeyDown(KeyCode.R)) 					{myState = States.corridor_1;}
+void sol() {
+	text.text = "The sidewinder emergency autopilot kicks in and starts to align your ship towards a " +
+							"little yellow star in the far far distance. Thrusters kicks of and you start to " +
+							"accelerate and as you hit critical speed the hyperdrive kicks in and BOOM you are on " +
+							"your way to SOL and civilised space..";
+  if (Input.GetKeyDown(KeyCode.L)) 					{myState = States.look;}
 }
 
-void stairs_2() {
-	text.text = "You feel smug for picking the closet door open, and are still armed with " +
-							"a hairclip (now badly bent). Even these achievements together don't give " +
-							"you the courage to climb up the staris to your death!\n\n" +
-							"Press R to Return to the corridor";
-  if (Input.GetKeyDown(KeyCode.R)) 					{myState = States.corridor_2;}
-}
 
-void cell () {
-	text.text = "You wake up feeling groggy, you start to get your bearing " +
-							"some dirty sheets on the bed, a mirror on the wall, and the door " +
-							"is locked from the outside.\n\n" +
-							"Press S to view Sheets, M to view Mirror and L to view Lock" ;
-  if 			(Input.GetKeyDown(KeyCode.S)) 	{myState = States.sheets_0;}
-	else if (Input.GetKeyDown(KeyCode.M)) 	{myState = States.mirror;}
-	else if (Input.GetKeyDown(KeyCode.L)) 	{myState = States.lock_0;}
-}
-
-void mirror() {
-	text.text = "The dirty old mirror on the wall seems loose.\n\n" +
-							"Press T to Take the mirror, or R to Return to cell" ;
-	if 			(Input.GetKeyDown(KeyCode.T)) 	{myState = States.cell_mirror;}
-	else if (Input.GetKeyDown(KeyCode.R)) 	{myState = States.cell;}
-}
-
-void cell_mirror() {
-	text.text = "You are still in your cell, and you STILL want to escape! There are " +
-							"some dirty sheets on the bed, a mark where the mirror was, " +
-							"and that pesky door is still there, and firmly locked!\n\n" +
-							"Press S to view Sheets, or L to view Lock" ;
-  if 			(Input.GetKeyDown(KeyCode.S)) 	{myState = States.sheets_1;}
-	else if (Input.GetKeyDown(KeyCode.L)) 	{myState = States.lock_1;}
-}
-
-void sheets_0 () {
-	text.text = "You can't believe you sleep in these things. Surely it's " +
-							"time somebody changed them. The pleasures of prison life " +
-							"I guess!\n\n" +
-							"Press R to Return to roaming your cell" ;
-  if (Input.GetKeyDown(KeyCode.R)) 				{myState = States.cell;}
-}
-
-void sheets_1() {
-	text.text = "Holding a mirror in your hand doesn't make the sheets look " +
-							"any better.\n\n" +
-							"Press R to Return to roaming your cell" ;
-	if (Input.GetKeyDown(KeyCode.R)) 				{myState = States.cell_mirror;}
-}
-
-void lock_0() {
-	text.text = "This is one of those button locks. You have no idea what the " +
-							"combination is. You wish you could somehow see where the dirty " +
-							"fingerprints were, maybe that would help.\n\n" +
-							"Press R to Return to roaming your cell" ;
-  if (Input.GetKeyDown(KeyCode.R)) 				{myState = States.cell;}
-}
-
-void lock_1() {
-	text.text = "You carefully put the mirror through the bars, and turn it round " +
-							"so you can see the lock. You can just make out fingerprints around " +
-							"the buttons. You press the dirty buttons, and hear a click.\n\n" +
-							"Press O to Open, or R to Return to your cell" ;
-  if 			(Input.GetKeyDown(KeyCode.O)) 	{myState = States.corridor_0;}
-	else if (Input.GetKeyDown(KeyCode.R)) 	{myState = States.cell_mirror;}
-}
-
-void corridor_0() {
-	text.text = "You're out of your cell, but not out of trouble." +
-							"You are in the corridor, there's a closet and some stairs leading to " +
-							"the courtyard. There's also various detritus on the floor.\n\n" +
-							"C to view the Closet, F to inspect the Floor, and S to climb the stairs";
-	if 			(Input.GetKeyDown(KeyCode.S)) 	{myState = States.stairs_0;}
-	else if (Input.GetKeyDown(KeyCode.F)) 	{myState = States.floor;}
-	else if (Input.GetKeyDown(KeyCode.C)) 	{myState = States.closet_door;}
-	}
 #endregion
 }
